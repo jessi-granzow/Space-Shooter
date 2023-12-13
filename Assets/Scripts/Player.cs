393478639ee191fddfe8d24e3a5596a9c278d822
Jessica Mathe <jessica.r.mathe@gmail.com>
2023-12-13 16:14:47 -0600

    prefab laser setup

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // public or private reference
+    // data type (int, flaot, bool, string)
+    // every variable has a name
+    // optional value assigned
+    [SerializeField]
+    private float _speed = 3.5f;
+
+    public float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current position = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculatedMovement();
+    }
+    void CalculatedMovement()
+    {
+  
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+
+        //vector3 direction = new Vector3(horizontalInput, VerticalInput, 0) cleaned up version  
+        // transform.Translate(direction * _speed * Time.deltaTime) cleaned up version 
+
+
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0);
+        }
+      
+        if (transform.position.x >= 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+
+
+
+    }
+
+
+
+
+
+}
+
+
+
+
